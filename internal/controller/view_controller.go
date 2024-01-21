f2b5a081cdf5580585af9138a51aee8b714ddd24
alvintanoto <alvintanotodev@gmail.com>
2024-01-21 22:37:35 +0700

    add not found logging

--- internal/controller/view_controller.go
+++ internal/controller/view_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"log"
 	"net/http"
 
 	"alvintanoto.id/blog-htmx-templ/view"
@@ -15,6 +16,7 @@ func NewViewController() *ViewController {
 
 func (vc ViewController) NotFoundViewHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("path:", r.URL.Path, "not found")
 		view.NotFoundPage().Render(r.Context(), w)
 	}
 }
