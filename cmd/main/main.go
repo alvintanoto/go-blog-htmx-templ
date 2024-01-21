780b6b14a40164172dd0701d6000cdaecaf8878e
alvintanoto <alvintanotodev@gmail.com>
2024-01-22 01:02:09 +0700

    add graceful shutdown

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"alvintanoto.id/blog-htmx-templ/internal/controller"
@@ -73,7 +76,6 @@ func main() {
 	app.InitializeController()
 	app.SetupRoutes()
 
-	// start the server here
 	server := http.Server{
 		Addr:         fmt.Sprintf(":%d", app.Configurations.Server.Port),
 		Handler:      app.Router,
@@ -82,8 +84,31 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	log.Println("starting server on", server.Addr)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("failed to start server: #%v", err)
-	}
+	// start the server here with goroutine so it doesnt block
+	go func() {
+		log.Println("starting server on", server.Addr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	server.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("server shutting down")
+	os.Exit(0)
 }
